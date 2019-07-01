af31d42e193c60d3a1ebe2b9c0ed62ac19a470ad
Adam Brown <adam.brown.betty@gmail.com>
2019-07-01 02:39:54 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 /*  assign an object by using curly brackets
-
+function takes 3 parameters
+object is updated with the KEY-VALUE pair using brackets because its a variable
 */
 
 var playlist = {artistName : 'songTitle'};
@@ -9,6 +10,11 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 return playlist;
 }
+
+
+/*   use DELETE to destructively remove the KEY artistName
+and retun the new (updated) playlist
+*/
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName];
